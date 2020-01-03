0997c2e9a3e11e383a642142ba809a5ef20eb142
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-03 09:15:01 +0100

    init commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request)
+    // here possible to add anything to the reqs editing the config, like headers
+    return request; // always return otherwise the req gets blocked
+}, error => {
+ console.log(error);
+ return Promise.reject(error);   
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response)
+    // here possible to add anything to the reqs editing the config, like headers
+    return response; // always return otherwise the req gets blocked
+}, error => {
+ console.log(error);
+ return Promise.reject(error);   
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
