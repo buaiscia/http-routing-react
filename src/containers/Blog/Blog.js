b648961eb3292befaf458cac5b57c4e38dce660f
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-03 09:26:46 +0100

    added defaults axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,7 @@ class Blog extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/postsss') // executed async, create promise
+        axios.get('/posts') // executed async, create promise
         .then((response) => {
             const posts = response.data.slice(0, 4);
             const updatedPosts = posts.map(post => {
