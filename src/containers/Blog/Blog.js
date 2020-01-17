382cb3841ac8075184a0720f6f3dfabce5935524
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-17 10:48:47 +0100

    switched stateful component from blogjs to postsjs

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 // import Post from '../../components/Post/Post';
 // import FullPost from '../../components/FullPost/FullPost';
 // import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts'
 
 class Blog extends Component {
 
-    state = {
-        posts : [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts') // executed async, create promise
-        .then((response) => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'alex'
-                }
-            })
-            this.setState({ posts: updatedPosts})
-            // console.log(response);
-        })                                                 // executed once promise resolves
-        .catch(error => {
-            // console.log(error);
-            this.setState({error: true})
-        })
-    }
-
-    postSelectedHandler = (id) => {
-         this.setState({selectedPostId: id})
-    }
-
+    
     render () {
 
-        let posts = <p style={{textAlign: 'center'}}>Something went wrogn</p>
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-            })
-        }
         
-
         return (
             <div className="Blog">
                 <header>
@@ -61,9 +23,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
                 {/* <section>
                     <FullPost 
                     id={this.state.selectedPostId}
