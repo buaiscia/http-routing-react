460024070c810aed1f929656837bc95d2a9002e4
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-17 11:01:14 +0100

    add routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 // import Post from '../../components/Post/Post';
 // import FullPost from '../../components/FullPost/FullPost';
 // import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 import Posts from './Posts/Posts'
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -23,7 +25,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+                <Route path='/' exact component={Posts} />
+                <Route path='/new-post' component={NewPost} />
+                {/* <Posts /> */}
                 {/* <section>
                     <FullPost 
                     id={this.state.selectedPostId}
